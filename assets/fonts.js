828a5921c855d9b0fe81250bf68bf711635fbe86
Echo <github@3kh0.net>
2025-04-25 16:45:33 -0400

    rebuild 2

---
+++ assets/fonts.js
@@ -0,0 +1,1 @@
+import '@fontsource/special-elite';
--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,5 @@
 import { defineConfig } from "vite";
-import tailwindcss from "@tailwindcss/vite";
+import tailwindcss from '@tailwindcss/vite'
 import htmlMinifier from 'vite-plugin-html-minifier';
 
 export default defineConfig({
